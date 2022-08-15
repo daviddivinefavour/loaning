60cd6e32a38f23d3d6043684fbc3690cd905ad9b
Divinefavour David <blazer0022@gmail.com>
2022-08-15 22:33:41 +0100

    installed development dependencies for unit testing

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+routes(app)
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const routes = require('./routes');
-const app = express();
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-routes(app)
-
+const app = require("./app");
 
 app.listen(process.env.PORT, ()=>{
      console.log('server is running on port: ' + process.env.PORT);
