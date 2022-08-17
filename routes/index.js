c279855b555c16c77c66e87a503a24e0939c766c
Divinefavour David <blazer0022@gmail.com>
2022-08-17 17:54:16 +0100

    __

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const transactionRoutes = require('./transactionRoutes');
 
 module.exports = (app)=>{
      app.get('/',(req,res)=>{
-          res.send('Alobam')
+          res.send('Welcome to lendsqr')
      })
      app.use('/api/v1', userRoutes);
      app.use('/api/v1', transactionRoutes);
