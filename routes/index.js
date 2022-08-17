8003e418b0167f62cee4298b2fa0daa9df7993f1
Divinefavour David <blazer0022@gmail.com>
2022-08-17 12:43:15 +0100

    temp landing page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const userRoutes = require('./userRoutes');
 const transactionRoutes = require('./transactionRoutes');
 
 module.exports = (app)=>{
+     app.get('/',(req,res)=>{
+          res.send('Alobam')
+     })
      app.use('/api/v1', userRoutes);
      app.use('/api/v1', transactionRoutes);
 }
